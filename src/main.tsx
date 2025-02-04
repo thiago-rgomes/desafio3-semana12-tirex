a7ee4fb393d5401772aac97b28a7d05ef958f58d
Thiago <tgomes_2@hotmail.com>
2025-02-04 10:27:57 -0300

    feat: add routing with react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  
+    <BrowserRouter>
+    </BrowserRouter>  
   </StrictMode>,
 )
---
+++ src/pages/app-routes.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
