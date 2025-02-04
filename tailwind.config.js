3cf52eb0a9e30072a4bbe1d3871be528d51a250c
Thiago <tgomes_2@hotmail.com>
2025-02-03 22:50:34 -0300

    Initial commit with project structure

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+
