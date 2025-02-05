ada89dd94242e4c6b8ab8f750cf3f7f1cd24f7cc
Thiago <tgomes_2@hotmail.com>
2025-02-05 10:31:20 -0300

    feat: add cartModal

---
+++ src/components/Header/Components/CartModal.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import bagIcon from "../../../assets/header/bag-icon.png"
+import crossIcon from "../../../assets/header/cross-icon.png"
+
+interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+}
+
+interface CartModalProps {
+  items: CartItem[];
+  onClose: () => void;
+  onRemoveItem: (id: number) => void;
+}
+
+export default function CartModal({ items, onClose, onRemoveItem }: CartModalProps) {
+  const [subtotal, setSubtotal] = useState(0);
+
+  useEffect(() => {
+    const total = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    
+    setSubtotal(total);
+  }, [items]);
+
+  return (
+    <div className="flex flex-col items-center fixed top-0 right-0 w-[417px] h-[746px] pt-[30px] bg-white shadow-lg">      
+      <div className="flex gap-[152px] items-center pb-[26px] w-[350px]">
+        <h2 className="text-[24px] font-semibold ">Shopping Cart</h2>
+        <button onClick={onClose}>
+          <img src={bagIcon} alt="cart-icon" />
+        </button>
+      </div>
+
+      <div className="w-[287px] h-[1px] bg-gray-200 self-start ml-[34px] mb-[42px]"></div>
+
+      
+      <div className="mt-4 space-y-4 overflow-y-auto h-2/3 w-[350px]">
+        {items.length === 0 ? (
+          <p className="text-gray-500">Seu carrinho está vazio.</p>
+        ) : (
+          items.map((item) => (
+            <div key={item.id} className="flex items-center justify-between w-[350px] h-[105px]">
+              <img src={item.image} alt={item.name} className="w-[108px] h-[105px] rounded flex justify-center items-center" />
+              <div className="ml-4">
+                <h3 className="text-base">{item.name}</h3>
+                <p className="text-base font-light">{item.quantity}
+                <span className="text-[12px] font-light ml-[15px] mr-[15px]">X</span>
+                <span className="text-[12px] font-medium text-[#B88E2F]">Rs. {item.price.toFixed(2)}</span>
+                </p>
+              </div>
+              <button onClick={() => onRemoveItem(item.id)}>
+                <img src={crossIcon} alt="cart-icon" />
+              </button>
+            </div>
+          ))
+        )}
+      </div>
+
+      
+      <div className="ml-[30px] pt-2 flex self-start gap-[100px]">
+        <p className="text-base flex self-start">Subtotal</p>
+        <p className="font-semibold text-[#B88E2F]">Rs. {subtotal.toFixed(2)}</p>
+      </div>
+
+
+      <div className="w-full h-[1px] bg-gray-200 self-start mt-[23px] mb-[26px]"></div>
+
+
+      <div className="flex justify-between items-center gap-[14px] mb-[20px]">
+        <Link to="/cartPage" className="flex justify-center items-center w-[87px] h-[30px] border border-black rounded-full text-[12px] hover:bg-gray-100">Cart</Link>
+        <Link to="/checkout" className="flex justify-center items-center w-[118px] h-[30px] border border-black rounded-full text-[12px] hover:bg-gray-100">Checkout</Link>
+        <Link to="/comparison" className="flex justify-center items-center w-[135px] h-[30px] border border-black rounded-full text-[12px] hover:bg-gray-100">Comparison</Link>
+      </div>
+    </div>
+  );
+}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,49 +3,74 @@ import { Link, useNavigate } from "react-router-dom"
 import siteLogo from '../../assets/header/site-logo.png';
 import userIcon from '../../assets/header/user-icon.png';
 import cartIcon from '../../assets/header/cart-icon.png';
+import CartModal from "./Components/CartModal";
 
 
 
 const Header = () => {
-    const [showCart, setShowCart] = useState(false);
+    
     const navigate = useNavigate();
+    const [showCart, setShowCart] = useState(false);
+    const [cartItems, setCartItems] = useState([
+      {
+        id: 1,
+        name: "Asgard Sofa",
+        price: 250000,
+        quantity: 1,
+        image: "/src/assets/header/cart-img1.png",
+      },
+      {
+        id: 2,
+        name: "Casaliving Wood",
+        price: 270000,
+        quantity: 2,
+        image: "/src/assets/header/cart-img2.png",
+      },
+    ]);
+
+      const removeItem = (id: number) => {
+        setCartItems(cartItems.filter(item => item.id !== id));
+      };
 
     
     return (
-        <div className="flex justify-between items-center w-full min-w-[950px] h-[100px] pl-[54px] pr-[100px]">
-            <div className="flex justify-center items-center gap-[5px]">
-                <img src={siteLogo} alt="site-logo" className="w-[50px] h-[32px]" />
-                <p className="font-montserrat text-[36px] font-bold">Furniro</p>
-            </div>
-            
-            <nav className="flex justify-between w-[430px] font-medium text-base">
-                <Link to="/">Home</Link>
-                <Link to="/shop">Shop</Link>
-                <Link to="">About</Link>
-                <Link to="/contact">Contact</Link>            
-            </nav>
 
-            <div className="flex gap-[34px]">
-                <img src={userIcon} alt="user-icon" className="w-[23px] h-[19px]" />
-
-                <div onMouseEnter={() => setShowCart(true)} onMouseLeave={() => setShowCart(false)}>
-                    <img src={cartIcon} alt="cart-icon" className="w-[23px] h-[19px]" onClick={() => navigate("/cartPage")} />
-                </div>
-            </div>
-
-            {/*Cart's Modal*/}
-            {showCart && (
-                <div className="absolute right-0 mt-2 w-64 bg-white shadow-lg rounded-lg p-4">
-                    <p className="text-gray-700">Seu carrinho está vazio.</p>
-                    <Link to="/cartPage" className="text-blue-500 text-sm hover:underline">
-                    Ver carrinho
-                    </Link>
-                </div>
-            )}
-        
+      <div className="flex justify-between items-center w-full min-w-[950px] h-[100px] pl-[54px] pr-[100px]">
+        <div className="flex justify-center items-center gap-[5px]">
+          <img src={siteLogo} alt="site-logo" className="w-[50px] h-[32px]" />
+          <p className="font-montserrat text-[36px] font-bold">Furniro</p>
         </div>
-    )
-    }
+            
+        <nav className="flex justify-between w-[430px] font-medium text-base">              
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shop</Link>
+          <Link to="">About</Link>
+          <Link to="/contact">Contact</Link>            
+        </nav>
+
+        <div className="flex gap-[34px]">   
+          <img src={userIcon} alt="user-icon" className="w-[23px] h-[19px]" />
+
+          <div onMouseEnter={() => setShowCart(true)} onMouseLeave={() => setShowCart(false)}>
+            <img src={cartIcon} alt="cart-icon" className="w-[23px] h-[19px]" onClick={() => navigate("/cartPage")} />
+          </div>
+        </div>
+
+        {/*Cart's Modal*/}
+        {showCart && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 z-40">
+
+            <div className="absolute right-0 mt-2 w-[417px] bg-white shadow-lg rounded-lg p-4 z-50 overflow-y-auto "
+              onMouseEnter={() => setShowCart(true)} 
+              onMouseLeave={() => setShowCart(false)}>
+            <CartModal items={cartItems} onClose={() => setShowCart(false)} onRemoveItem={removeItem}/>
+            </div>
+          </div>             
+        )}
+        
+      </div>
+  )
+  }
 
 export default Header
 
