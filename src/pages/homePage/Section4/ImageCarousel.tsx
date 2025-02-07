05ee74d46c9d4f5c75b02b4aa663e855566bc17e
Thiago <tgomes_2@hotmail.com>
2025-02-06 22:02:20 -0300

    feat: add section 4 to homepage

---
+++ src/pages/homePage/Section4/ImageCarousel.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import { ArrowRight } from "lucide-react";
+
+const images = [
+  { id: 1, src: "/src/assets/homepage/section4/img1.png", title: "Inner Peace", category: "Bed Room" },
+  { id: 2, src: "/src/assets/homepage/section4/img2.png", title: "Cozy Vibes", category: "Living Room" },
+  { id: 3, src: "/src/assets/homepage/section4/img3.png", title: "Minimalist", category: "Dining Room" },
+  { id: 4, src: "/src/assets/homepage/section4/img1.png", title: "Great Style", category: "Workspace" },
+];
+
+export default function Carousel() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <div className="max-w-6xl min-w-[1100px] mx-auto px-4">
+      <Splide
+        options={{
+          type: "slide",
+          perPage: 3, 
+          perMove: 1, 
+          gap: "1rem",
+          pagination: true,
+          arrows: true,
+          focus: "center",
+          trimSpace: false, 
+          breakpoints: {
+            1024: { perPage: 2 },
+            768: { perPage: 1 },
+          },
+        }}
+        onMove={(splide) => setActiveIndex(splide.index)}
+      >
+        {images.map((image, index) => (
+          <SplideSlide key={image.id}>
+            <div className={`relative group transition-all duration-300 ${index === activeIndex ? "scale-105" : "scale-95"}`}>
+              <img src={image.src} alt={image.title} className="w-[404px] h-[582px]" />
+                           
+              {index === activeIndex && (
+                <div className="absolute flex flex-col justify-center w-[217px] h-[130px] bottom-4 left-4 bg-white opacity-70 p-3 shadow-md">
+                  <span className="text-[#616161]">{`0${index + 1} — ${image.category}`}</span>
+                  <h3 className="font-semibold text-[28px]">{image.title}</h3>
+
+                  <button className="absolute right-[-40px] bottom-[20px] translate-y-1/2 bg-yellow-600 p-3 shadow-md">
+                    <ArrowRight size={16} className="text-white" />
+                  </button>
+                </div>
+              )}
+            </div>
+          </SplideSlide>
+        ))}
+        <style>{".splide__arrow--prev { display: none !important; }"}</style>
+      </Splide>
+    </div>
+  );
+}
---
+++ src/pages/homePage/Section4/Section4.tsx
@@ -0,0 +1,22 @@
+import ImageCarousel from "./ImageCarousel"
+
+
+const Section4 = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-[670px] pl-[100px] py-[44px] gap-[42px] bg-[#FCF8F3]">
+      <div>
+        <h3 className="font-bold text-[40px] text-[#3A3A3A] mb-[7px]">50+ Beautiful rooms inspiration</h3>
+        <p className="font-medium text-[#616161] mb-[25px]">Our designer already made a lot of beautiful prototipe of rooms that inspire you</p>
+        <button className="w-[176px] h-[48px] font-semibold text-white bg-[#B88E2F]">Explore More</button>
+      </div>
+
+      <div className="overflow-visible mr-[100px]">
+        <ImageCarousel></ImageCarousel>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Section4
